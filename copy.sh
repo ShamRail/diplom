1d096b26fe4f94043bf6e9072926df7280faf922
ShamRail <rail1999g102r@mail.ru>
2021-12-19 18:27:28 +0500

    Loaded helping copy script

---
+++ copy.sh
@@ -0,0 +1,6 @@
+rsync -r --exclude=admin-client/node_modules \
+        --exclude=runner-client/node_modules \
+        --exclude=admin/target \
+        --exclude=builder/target \
+        --exclude=runner/target \
+        /mnt/c/Users/rail/Desktop/diplom diplom/
